5f518c78a9f237dfd0bf2e2f6b29d728a176dac4
Josh Cormier <you@example.com>
2020-01-20 14:58:46 -0500

    Changed stack to store only the number

--- generate.js
+++ generate.js
@@ -7,19 +7,17 @@ let numbers = {
 };
 
 
-
+const key = "CallStack";
 let callStack = []
 
-function equal (n1, n2){
-    return n1.innerHTML === n2.innerHTML;
-} 
+
 
 function find(n1){
     let i = 0;
     for(; i < callStack.length; i++){
-        if(equal(n1, callStack[i]))
+        if(n1 === callStack[i] )
         {
-            return i;
+            return i
         }
     }
     return -1;
@@ -31,43 +29,64 @@ function CalledNumber() {
     console.log(callStack)
     if (callStack.length === 0) {
         this.classList.add("purple");
-        callStack.push(this);
+        callStack.push(this.innerHTML);
     } else if(hasColor) 
     {
-       let i = find(this)
+       let i = find(this.innerHTML)
        callStack.splice(i, 1)
+       let top = callStack.length -1;
+       if(top >= 0)
+       {
+        let old = _getNumber(callStack[top])
+        old.classList.remove("red")
+        old.classList.add("purple")
+       }
        this.classList.remove("red")
        this.classList.remove("purple")
+       
     }
     
     else {
         
         this.classList.add("purple");
-        let top = callStack.length - 1
-        callStack[top].classList.add("red")
-        callStack[top].classList.remove("purple")
-        callStack.push(this)
+        let top = callStack.length -1
+        let old = _getNumber(callStack[top])
+        old.classList.add("red")
+        old.classList.remove("purple")
+        callStack.push(this.innerHTML)
     }
+    store_in_session()
+}
+//Stores callstack in session variable
+function store_in_session(){
+    sessionStorage.setItem(key, callStack)
+}
+function get_from_session(){
+    return sessionStorage.getItem(key)
+}
+function remove_from_session(){
+    sessionStorage.removeItem(key)
 }
 
 function undo() {
-    alert("hello")
-    debugger;
     let top = callStack.length - 1;
     callStack[top].classList.remove("purple")
+    callStack.splice(top, 1)
     top = callStack.length - 1;
     callStack[top].classList.add("purple")
     callStack[top].classList.remove("red")
 }
 
 //creates a new element and adds a css class to it
-function newElement(element, cssClass) {
+function _newElement(element, cssClass) {
     let x = document.createElement(element);
     x.classList.add(cssClass);
     return x;
 }
 
-
+function _getNumber(num){
+    return document.querySelector("#n" + num)
+}
 
 
 function create_board() {
@@ -75,10 +94,10 @@ function create_board() {
     for (let letter in numbers) {
 
         //create the row for the table
-        let node = newElement("div", "row");
+        let node = _newElement("div", "row");
 
         //create the header column for the table and add the header class to it
-        let header = newElement("div", "col");
+        let header = _newElement("div", "col");
         header.classList.add("header");
         //set the text to be a letter in BINGO
         header.innerHTML = letter;
@@ -91,7 +110,8 @@ function create_board() {
             //store the number in a variable
             let num = numbers[letter][i];
             //create the column, then add the number-container class
-            let col = newElement("div", "col");
+            let col = _newElement("div", "col");
+            col.id = "n"+ numbers[letter][i];
             col.classList.add("number-container");
 
             //set the text equal to the number.
@@ -113,10 +133,21 @@ function create_board() {
     }
 }
 
+function board_reload(){
+    debugger;
+    let called = get_from_session()
+    if(called === null || called.length === 0) throw RangeError("No numbers where called")
+    remove_from_session()
+    for(let i in called){
+        let el = _getNumber(i)
+        el.onclick()
+
+    }
+    
+}
+
 
 function resetNumbers() {
-    alert("hello")
-    debugger;
     //selects all the divs with the class red
     let numbers = document.querySelectorAll("div.number-container");
     console.log(numbers);
@@ -126,5 +157,6 @@ function resetNumbers() {
         numbers[i].classList.remove("red");
         numbers[i].classList.remove("purple");
     }
-    lastCall = null;
+    callStack = [];
+    remove_from_session()
 }
